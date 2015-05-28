08f05c7f63c95f78aea281a0dbc6b936aebfa95c
Fred Yang <fredyang@blueprint.com>
2015-05-28 14:36:48 -0400

    add layer1

---
+++ layer1/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace layer1
+{
+    public static class Animal
+    {
+        public static void Shout()
+        {
+            Console.WriteLine("woo...");
+        }
+    }
+}
